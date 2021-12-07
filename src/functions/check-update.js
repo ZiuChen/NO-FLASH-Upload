56ae4c5e464cde52ebb19bb781122ba95c0af167
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2021-12-07 22:17:59 +0800

    增加脚本内检查更新功能

---
+++ src/functions/check-update.js
@@ -0,0 +1,41 @@
+import PopNotify from "../js/PopNotify"
+const { version } = require('../../package.json')
+
+async function getLastestVersion() {
+    const GreasyUrl = "https://greasyfork.org/zh-CN/scripts/432056"
+    let res = await fetch(GreasyUrl)
+        .then(response => {
+            return response.text().then(DOMString => {
+                let obj = document.createElement("div")
+                obj.innerHTML = DOMString
+                return obj.querySelectorAll('.script-show-version>span')[1].textContent
+            })
+        })
+        .catch(error => { console.error(error) })
+    return res
+}
+
+async function checkVersion() {
+    if (location.href.indexOf('welcomepage/student/index.jsp') == -1) {
+        // 只发welcome页的一次请求
+        return
+    }
+    getLastestVersion().then(res => {
+        let weightLastest = 0
+        let weightNow = 0
+        res.split('.').reverse().forEach((value, index) => {
+            weightLastest += (index + 1) * value
+        })
+        version.split('.').reverse().forEach((value, index) => {
+            weightNow += (index + 1) * value
+        })
+        if (weightLastest > weightNow) {
+            console.log('need update');
+            PopNotify.show(`有新版本[${res}]`, "点击安装新版本", "success", "90", () => {
+                window.open(`https://greasyfork.org/zh-CN/scripts/432056`)
+            });
+        } else console.log('version Checked');
+    })
+}
+
+export default checkVersion
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import getHomeworkInfo from "./functions/listpage-get-homework-info"
 import getReminder from "./functions/get-reminder";
 import getResources from "./functions/get-resources";
 import judgeOnceSubmit from "./functions/only-once-commit";
+import checkUpdate from "./functions/check-update";
 import uploadBoxCSS from './style/css/upload-doms.css';
+import popNotify from "./style/css/PopNotify.css"
 
 apppendTags();
 fileUploader();
@@ -13,3 +15,4 @@ getReminder();
 getResources();
 getHomeworkInfo();
 judgeOnceSubmit();
+checkUpdate();
