f06d83392db81b540c9758c4aa7f9b2affe83db3
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-02-23 16:23:28 +0800

    loadCSS 从CSS文件加载样式

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ import { createApp } from "vue";
 import Main from "./components/Main.vue";
 // import Notify from "./components/Notify.vue";
 import router from "./route";
+import mainStyle from "./style/MainStyle"
+import loadCSS from "./function/LoadCSS"
 
 const href = window.location.href;
 
 if (href.indexOf("main.jsp") !== -1) {
   init();
+  loadCSS(mainStyle)
   createApp(Main).use(router).mount("body");
 } else if (href.indexOf("course_meswrap.jsp") !== -1) {
+  // notify detail
   // init();
   // createApp(Notify).mount("body");
 }
@@ -19,13 +23,4 @@ function init() {
     .forEach((n: HTMLElement) => {
       n.remove();
     });
-  document.querySelectorAll("body").forEach((n: HTMLBodyElement) => {
-    n.style.margin = "0";
-    n.style.overflowX = "hidden";
-    n.style.overflowY = "hidden";
-    n.style.fontFamily =
-      "Helvetica, Tahoma, Arial, 'Heiti SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei'";
-    n.style.fontWeight = "lighter";
-    n.style.textShadow = "0px 1px 1px #cccccc";
-  });
 }
---
+++ src/style/MainStyle.ts
@@ -0,0 +1,12 @@
+const style = /* css */ `
+body {
+    margin: 0;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    font-family: Helvetica, Tahoma, Arial, 'Heiti SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei';
+    font-weight: lighter;
+    text-shadow: 0px 1px 1px #cccccc;
+  }
+`;
+
+export default style;
