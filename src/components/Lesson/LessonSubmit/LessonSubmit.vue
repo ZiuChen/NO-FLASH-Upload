a0323ecbc1c408cfefd7b8f5b9564dd31d7b9cb5
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-02-27 23:31:11 +0800

    submit页中加入hwtlist 还存在问题

--- src/components/Lesson/LessonSubmit/LessonSubmit.vue
+++ src/components/Lesson/LessonSubmit/LessonSubmit.vue
@@ -1,23 +1,32 @@
 <template>
-  <el-card shadow="hover">
-    <template #header>
-      <div class="card-header">
-        <span>提交作业</span>
-      </div>
-    </template>
-    <hwt-info :propHwtContent="hwtContent"></hwt-info>
-    <hwt-editor :propHwtContent="hwtContent"></hwt-editor>
-  </el-card>
+  <el-row>
+    <el-col :span="16">
+      <el-card shadow="hover">
+        <template #header>
+          <div class="card-header">
+            <span>提交作业</span>
+          </div>
+        </template>
+        <hwt-info :propHwtContent="hwtContent"></hwt-info>
+        <hwt-editor :propHwtContent="hwtContent"></hwt-editor>
+      </el-card>
+    </el-col>
+    <el-col :span="8">
+      <hwt-list></hwt-list>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 import getInfo from "../../../ts/GetInfo";
 import HwtInfo from "./HwtInfo.vue";
 import HwtEditor from "./HwtEditor.vue";
+import HwtList from "../../WelcomePage/HwtList.vue";
 export default {
   components: {
     HwtInfo,
     HwtEditor,
+    HwtList,
   },
   data() {
     return {
@@ -44,6 +53,10 @@ export default {
 </script>
 
 <style scoped>
+.hwt-list .el-table {
+  height: 100%;
+}
+
 .el-card {
   min-height: 100%;
   height: 100%;
