eeed82a401b2112261d462765157c51a43c96b11
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-03-01 10:29:17 +0800

    配置更新

--- config/metadata.js
+++ config/metadata.js
@@ -1,22 +1,27 @@
-const { author, repository, version } = require("../package.json");
+const { author, repository, version, description } = require("../package.json");
 
 module.exports = {
   name: "免Flash文件上传",
   "name:en": "NO-FLASH-Upload",
-  description:
-    "无需调用Flash，从课程平台上传附件，不必为了传作业多装一个浏览器！",
+  description: description,
   "description:en":
-    "No need to call Flash, upload accessories from the course platform.",
+    "Beijing Jiaotong University curriculum platform function enhancements, information aggregation, accessories uploading, allowing you to efficient course information.",
+  updateURL:
+    "https://raw.githubusercontent.com/ZiuChen/NO-FLASH-Upload/v2.x-Vue/publish/index.prod.user.js",
+  downloadURL:
+    "https://raw.githubusercontent.com/ZiuChen/NO-FLASH-Upload/v2.x-Vue/publish/index.prod.user.js",
   version: version,
   author: author,
-  namespace: "https://greasyfork.org/zh-CN/users/605474",
-  icon: "https://gitee.com/ziuc/utool-filebed/raw/master/20210514-231824-0795.png",
   source: repository.url,
+  license: "MIT",
   match: ["*://cc.bjtu.edu.cn:81/meol*"],
+  namespace: "https://greasyfork.org/zh-CN/users/605474",
   require: [
     "https://cdn.jsdelivr.net/npm/wangeditor@latest/dist/wangEditor.min.js",
     "https://cdn.jsdelivr.net/npm/gbk.js@0.3.0/dist/gbk.min.js",
+    "https://cdn.jsdelivr.net/npm/mdui@1.0.2/dist/js/mdui.min.js",
   ],
+  connect: ["raw.githubusercontent.com", "github.com", "cdn.jsdelivr.net"],
+  icon: "https://gitee.com/ziuc/utool-filebed/raw/master/20210514-231824-0795.png",
   grant: [],
-  license: "MIT",
 };
