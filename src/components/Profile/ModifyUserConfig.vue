c7af69cf065558acb5c9d3cb28c116e42aad5e31
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-03-12 19:48:34 +0800

    调整设置页样式

---
+++ src/components/Profile/ModifyUserConfig.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-card>
+    <template #header>
+      <div class="card-header">
+        <span>用户设置</span>
+      </div>
+    </template>
+    <div :key="config.name" v-for="config in inputNumberConfigs">
+      <span>{{ config.name }}</span>
+      <el-input-number
+        v-model="num"
+        :min="1"
+        :max="10"
+        @change="handleChange"
+      />
+    </div>
+  </el-card>
+</template>
+
+<script>
+import ConfigOperations from "../../ts/Config/ConfigOperations";
+
+export default {
+  data() {
+    return {
+      inputNumberConfigs:
+        ConfigOperations.readUserConfigWithFilter("input-number"),
+      switchConfigs: ConfigOperations.readUserConfigWithFilter("switch"),
+    };
+  },
+};
+</script>
+
+<style scoped></style>
--- src/components/Profile/ProfileIndex.vue
+++ src/components/Profile/ProfileIndex.vue
@@ -1,113 +1,18 @@
 <template>
-  <el-card class="box-card" shadow="hover">
-    <template #header>
-      <div class="card-header">
-        <span>主页卡片显示设置</span>
-      </div>
-    </template>
-    <div class="slider-demo-block" :key="key.id" v-for="key in cardSizeConfigs">
-      <span class="demonstration">{{ cardNameformatter(key.id) }}</span>
-      <el-slider
-        v-model="key.value"
-        :min="0"
-        :max="24"
-        :step="1"
-        show-stops
-        :marks="marks"
-      >
-      </el-slider>
-    </div>
-  </el-card>
-  <div class="button-div">
-    <el-button @click="saveConfig">保存</el-button>
-    <el-button @click="restoreConfig">重置</el-button>
-  </div>
+  <modify-user-config></modify-user-config>
 </template>
 
 <script>
-import configOperations from "../../ts/Config/ConfigOperations";
+import ModifyUserConfig from "./ModifyUserConfig.vue";
 export default {
+  components: {
+    ModifyUserConfig,
+  },
   data() {
-    return {
-      marks: {
-        0: "不显示",
-        6: "6",
-        12: "12",
-        18: "18",
-        24: "24",
-      },
-      userConfig: configOperations.readUserConfig(),
-      cardSizeConfigs: configOperations.readUserConfigWithFilter("card-size"), // Array
-    };
+    return {};
   },
-  methods: {
-    saveConfig() {
-      this.cardSizeConfigs.forEach((config) => {
-        configOperations.setUserConfig(config.id, config.value);
-      });
-      let notify = ElNotification({
-        title: "免Flash文件上传",
-        type: "success",
-        message: `设置已更新`,
-        onClick: () => {
-          notify.close();
-        },
-      });
-    },
-    restoreConfig() {
-      this.cardSizeConfigs.forEach((config, index) => {
-        this.cardSizeConfigs[index].value = config.default;
-        configOperations.setUserConfig(config.id, config.default);
-      });
-      this.saveConfig();
-    },
-
-    cardNameformatter(id) {
-      switch (id) {
-        case "hwt-list-card-size":
-          return "作业列表";
-        case "notify-list-card-size":
-          return "通知列表";
-        case "lesson-list-card-size":
-          return "课程列表";
-        case "inform-list-card-size":
-          return "系统通知";
-        case "user-info-card-size":
-          return "用户信息";
-      }
-    },
-  },
+  methods: {},
 };
 </script>
 
-<style scoped>
-.slider-demo-block {
-  display: flex;
-  align-items: center;
-}
-
-.slider-demo-block .el-slider {
-  margin-top: 0;
-  margin-left: 12px;
-}
-
-.slider-demo-block .demonstration {
-  font-size: 14px;
-  color: var(--el-text-color-secondary);
-  line-height: 44px;
-  flex: 1;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  margin-bottom: 0;
-}
-
-.slider-demo-block .demonstration + .el-slider {
-  flex: 0 0 70%;
-}
-
-.button-div {
-  padding-top: 25px;
-  text-align: center;
-}
-</style>
+<style scoped></style>
