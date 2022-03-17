9e17d8f6b8185462fc19b49f1cef591bf378ae33
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-03-17 19:33:54 +0800

    IMPROVE: 弹窗细节调整

--- src/ts/Requests/getLoginStatus.ts
+++ src/ts/Requests/getLoginStatus.ts
@@ -1,4 +1,3 @@
-import { WebpackOptionsValidationError } from "webpack";
 import getUserInfo from "./getUserInfo";
 
 export default async function getLoginStatus() {
@@ -8,7 +7,7 @@ export default async function getLoginStatus() {
         title: "免Flash文件上传",
         type: "warning",
         duration: 0,
-        message: `登录状态已过期，请重新登录`,
+        message: `获取用户信息失败，请检查登录状态是否过期`,
         onClick: () => {
           notify.close();
           location.href = `http://cc.bjtu.edu.cn:81/meol/index.do`;
