63deb1b4a3a8899355e77d3e33cd5bb82d33001e
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-03-19 10:11:28 +0800

    sendRequest增加超时响应

--- src/ts/SendRequest.ts
+++ src/ts/SendRequest.ts
@@ -1,53 +1,58 @@
-import getErrorTips from "./GetErrorTips";
+import log from "./Log";
 
 async function sendRequest(url: string, callBack?: Function, options?: Object) {
-  let res = await fetch(url, options)
-    .then((response) => {
-      if (response.ok === false) {
-        let notify = ElNotification({
-          title: "免Flash文件上传",
-          type: "error",
-          dangerouslyUseHTMLString: true,
-          message: `吖，请求</br><a href="${url}">${url}</a></br>时出错了，${getErrorTips(
-            url
-          )}请检查网络后重试或联系开发者`,
-          duration: 0,
-          onClick: () => {
-            // window.open(url);
-            notify.close();
-          },
-        });
-        throw Error(
-          `Request to ${url} rejected, with status ${response.status}`
-        );
-      }
+  log("sendRequest", `发送请求: ${url}`);
+  return await Promise.race([
+    fetch(url, options).then((response) => {
+      // 没有处理DOM的回调则直接返回
       if (callBack === undefined) return response;
       else {
         return response
           .blob()
           .then((blob) => {
-            return new Promise((resolve) => {
-              let reader = new FileReader();
-              reader.readAsText(blob, "GBK");
-              reader.onload = () => {
-                let data: any = reader.result;
-                let dom = new window.DOMParser().parseFromString(
-                  data,
-                  "text/html"
-                );
-                resolve(dom);
-              };
+            return blob2txt(blob).then((text: string) => {
+              return new window.DOMParser().parseFromString(text, "text/html");
             });
           })
           .then((res) => {
             return callBack(res);
           });
       }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  return res;
+    }),
+    new Promise((resovle, reject) => {
+      setTimeout(() => {
+        reject(new Error(`请求超时: ${url}`));
+      }, 5000);
+    }),
+  ]).then((res) => {
+    if (res === undefined) {
+      // return undefined
+      log("sendRequest", `返回值为 undefined: ${url}`, "error");
+    } else {
+      if (res.ok !== undefined) {
+        // Response
+        if (res.ok === true) {
+          log("sendRequest", `请求完成: ${url}`, "success");
+        } else {
+          log("sendRequest", `请求出错: ${url}`, "error");
+        }
+      } else {
+        log("sendRequest", `请求完成: ${url}`, "success");
+      }
+    }
+    return res;
+  });
+}
+
+async function blob2txt(blob: Blob) {
+  return new Promise((res, rej) => {
+    let reader = new FileReader();
+    reader.readAsText(blob, "GBK");
+    reader.onload = (e) => {
+      res(e.target.result);
+    };
+    reader.onerror = rej;
+  });
 }
 
 export default sendRequest;
