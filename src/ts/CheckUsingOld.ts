9cc2b530099c7d4d5731cdfdc1d152f353a8fb2a
ZiuChen <ZiuChen@outlook.com>
2022-04-22 17:37:34 +0800

    旧版验证弹窗逻辑修改

--- src/ts/CheckUsingOld.ts
+++ src/ts/CheckUsingOld.ts
@@ -2,17 +2,24 @@ import ConfigOperations from "./Config/ConfigOperations";
 
 function checkUsingOld() {
   if (ConfigOperations.readUserConfig()["config-back-to-old"].value) {
-    // true
-    let notify = ElNotification({
-      title: "免Flash文件上传",
+    // using old version
+    ElMessage({
       type: "warning",
-      message: `正在使用旧版，点此回到新版界面`,
-      onClick: () => {
-        ConfigOperations.setUserConfig("config-back-to-old", false);
-        window.location.reload();
-        notify.close();
-      },
+      message: "正在使用旧版",
     });
+    if (location.href.indexOf("personal.do") !== -1) {
+      // only show notification in personal.do page
+      let notify = ElNotification({
+        title: "免Flash文件上传",
+        type: "warning",
+        message: `正在使用旧版，点此回到新版界面`,
+        onClick: () => {
+          ConfigOperations.setUserConfig("config-back-to-old", false);
+          window.location.reload();
+          notify.close();
+        },
+      });
+    }
     return true;
   } else {
     return false;
