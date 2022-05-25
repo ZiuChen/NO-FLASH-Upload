26da3280f5de23d99c2353f56a949d1d7f9e8f23
ZiuChen <ZiuChen@outlook.com>
2022-05-25 23:13:31 +0800

    FEAT: 关于页增加Gitee的访问入口

--- src/components/About/ScriptInfoCard.vue
+++ src/components/About/ScriptInfoCard.vue
@@ -35,7 +35,28 @@
               ></path>
             </svg>
           </el-icon>
-          主页
+          Github
+        </el-button>
+        <el-button class="inner-button" @click="handleButtonClick('gitee')">
+          <el-icon :size="iconSize">
+            <svg
+              t="1653490652186"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="20876"
+              width="200"
+              height="200"
+            >
+              <path
+                d="M512 1024C229.222 1024 0 794.778 0 512S229.222 0 512 0s512 229.222 512 512-229.222 512-512 512z m259.149-568.883h-290.74a25.293 25.293 0 0 0-25.292 25.293l-0.026 63.206c0 13.952 11.315 25.293 25.267 25.293h177.024c13.978 0 25.293 11.315 25.293 25.267v12.646a75.853 75.853 0 0 1-75.853 75.853h-240.23a25.293 25.293 0 0 1-25.267-25.293V417.203a75.853 75.853 0 0 1 75.827-75.853h353.946a25.293 25.293 0 0 0 25.267-25.292l0.077-63.207a25.293 25.293 0 0 0-25.268-25.293H417.152a189.62 189.62 0 0 0-189.62 189.645V771.15c0 13.977 11.316 25.293 25.294 25.293h372.94a170.65 170.65 0 0 0 170.65-170.65V480.384a25.293 25.293 0 0 0-25.293-25.267z"
+                fill=""
+                p-id="20877"
+              ></path>
+            </svg>
+          </el-icon>
+          Gitee
         </el-button>
         <el-button class="inner-button" @click="handleButtonClick('donate')">
           <el-icon :size="iconSize">
@@ -82,8 +103,12 @@
           >
           <template #dropdown>
             <el-dropdown-menu>
-              <el-dropdown-item :command="'github'">Github Issue</el-dropdown-item>
-              <el-dropdown-item :command="'support'">腾讯兔小巢</el-dropdown-item>
+              <el-dropdown-item :command="'github'"
+                >Github Issue</el-dropdown-item
+              >
+              <el-dropdown-item :command="'support'"
+                >腾讯兔小巢</el-dropdown-item
+              >
               <el-dropdown-item :command="'qq'">加入反馈群</el-dropdown-item>
             </el-dropdown-menu>
           </template>
@@ -138,13 +163,13 @@
 </template>
 
 <script>
-import config from "../../ts/Config/Config"
-import CheckUpdate from "../../ts/CheckUpdate"
-import getVersionInfo from "../../ts/GetVersionInfo"
-import API from "../../ts/API"
+import config from "../../ts/Config/Config";
+import CheckUpdate from "../../ts/CheckUpdate";
+import getVersionInfo from "../../ts/GetVersionInfo";
+import API from "../../ts/API";
 export default {
   mounted() {
-    this.getVersionInfo()
+    this.getVersionInfo();
   },
   data() {
     return {
@@ -152,36 +177,38 @@ export default {
       needUpdate: false,
       iconSize: 18,
       donateUrl: `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/doc/img/Buy%20me%20a%20coffee.png`,
-    }
+    };
   },
   methods: {
     handleButtonClick(type) {
       if (type === "github") {
-        window.open(this.config.githubURL)
+        window.open(this.config.githubURL);
+      } else if (type === "gitee") {
+        window.open(this.config.giteeURL);
       } else if (type === "donate") {
-        window.open(this.donateUrl)
+        window.open(this.donateUrl);
       } else if (type === "notify") {
-        API.getLastestScriptNotify(true)
+        API.getLastestScriptNotify(true);
       } else if (type === "support") {
         // drop-down items
-        window.open(`https://support.qq.com/products/395800`)
+        window.open(`https://support.qq.com/products/395800`);
       } else if (type === "qq") {
         // drop-down items
         window.open(
           `https://qm.qq.com/cgi-bin/qm/qr?k=9qfHKTaQuWqYN1ys1yiQPdJ4iIlHwgL5&jump_from=webapi`
-        )
+        );
       }
     },
     CheckUpdate() {
-      CheckUpdate()
+      CheckUpdate();
     },
     async getVersionInfo() {
       await getVersionInfo().then((res) => {
-        this.needUpdate = res.need
-      })
+        this.needUpdate = res.need;
+      });
     },
   },
-}
+};
 </script>
 
 <style scoped>
--- src/ts/Config/Config.ts
+++ src/ts/Config/Config.ts
@@ -1,10 +1,11 @@
-const { version, repository, description } = require("../../../package.json")
-const updateURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/publish/index.prod.user.js`
-const updateInfo = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/package.json`
-const updateLOG = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/doc/update_log.md`
-const notifyURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/src/notify.json`
-const pluginsURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/src/plugins.json`
-const pluginsBaseURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/plugins/`
+const { version, repository, description } = require("../../../package.json");
+const giteeURL = `https://gitee.com/ziuc/NO-FLASH-Upload`;
+const updateURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/publish/index.prod.user.js`;
+const updateInfo = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/package.json`;
+const updateLOG = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/doc/update_log.md`;
+const notifyURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/src/notify.json`;
+const pluginsURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/src/plugins.json`;
+const pluginsBaseURL = `https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/plugins/`;
 
 const userConfig = {
   "data-last-read-notify": {
@@ -119,11 +120,12 @@ const userConfig = {
     id: "config-userinfo-show",
     type: "switch",
   },
-}
+};
 
 export default {
   version: version,
   githubURL: repository.url,
+  giteeURL: giteeURL,
   description: description,
   updateURL: updateURL,
   updateInfo: updateInfo,
@@ -132,4 +134,4 @@ export default {
   pluginsURL: pluginsURL,
   pluginsBaseURL: pluginsBaseURL,
   userConfig: userConfig,
-}
+};
