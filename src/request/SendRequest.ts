4214e867a9746206b209cd26e6374418d09dbdab
ZiuChen <457353192@qq.com>
2022-06-22 19:32:21 +0800

    refactor: 简化SendRequest逻辑

--- src/request/SendRequest.ts
+++ src/request/SendRequest.ts
@@ -3,43 +3,26 @@ import config from "../hooks/Config/Config";
 
 async function sendRequest(url: string, callBack?: Function, options?: Object) {
   log("sendRequest", `发送请求: ${url}`);
-  return await Promise.race([
-    fetch(url, options).then((response) => {
+  return fetch(url, options)
+    .then((response) => {
       // 没有处理DOM的回调则直接返回
       if (callBack === undefined) return response;
       else {
         return response
           .blob()
-          .then((blob) => {
-            return blob2txt(blob).then((text: string) => {
-              return new window.DOMParser().parseFromString(text, "text/html");
-            });
-          })
-          .then((res) => {
-            return callBack(res);
-          });
+          .then((blob) => blob2dom(blob))
+          .then((res) => callBack(res));
       }
-    }),
-    new Promise((resovle, reject) => {
-      setTimeout(() => {
-        reject(new Error(`请求超时: ${url}`));
-      }, 5000);
-    }),
-  ])
+    })
     .then((res) => {
       if (res === undefined) {
         // return undefined
         log("sendRequest", `返回值为 undefined: ${url}`, "error");
       } else {
-        if (res.ok !== undefined) {
-          // Response
-          if (res.ok === true) {
-            log("sendRequest", `请求完成: ${url}`, "success");
-          } else {
-            log("sendRequest", `请求出错: ${url}`, "error");
-          }
-        } else {
+        if (res.ok) {
           log("sendRequest", `请求完成: ${url}`, "success");
+        } else {
+          log("sendRequest", `请求出错: ${url}`, "error");
         }
       }
       return res;
@@ -58,12 +41,14 @@ async function sendRequest(url: string, callBack?: Function, options?: Object) {
     );
 }
 
-async function blob2txt(blob: Blob) {
+async function blob2dom(blob: Blob) {
   return new Promise((res, rej) => {
     let reader = new FileReader();
     reader.readAsText(blob, "GBK");
     reader.onload = (e) => {
-      res(e.target.result);
+      const text = e.target.result as string;
+      const dom = new window.DOMParser().parseFromString(text, "text/html");
+      res(dom);
     };
     reader.onerror = rej;
   });
