2e1b434996aca597ff1bba1ab3b6303640dd6c19
ZiuChen <457353192@qq.com>
2022-06-22 21:00:49 +0800

    fix: 导入el-loading样式

--- src/global/RegisterApp.ts
+++ src/global/RegisterApp.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "../components/App.vue";
 import router from "../route";
 import { ElLoading } from "element-plus/lib/components/loading/index";
+import "element-plus/theme-chalk/el-loading.css";
 
 const registerApp = () => {
   const app = createApp(App);
