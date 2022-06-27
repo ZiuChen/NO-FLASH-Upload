7398ba92805f088dddfa985e04f2e274d5f63320
ZiuChen <457353192@qq.com>
2022-06-27 12:52:26 +0800

    chore: 简化组件注册逻辑

--- src/global/RegisterApp.ts
+++ src/global/RegisterApp.ts
@@ -3,7 +3,7 @@ import App from "../components/App.vue";
 import router from "../route";
 import { registerElementPlus } from "./RegisterElementPlus";
 import { registerIcons } from "./RegisterIcons";
-import { ElLoading } from "element-plus/lib/components/loading/index";
+import { ElLoading } from "element-plus";
 import "element-plus/theme-chalk/el-loading.css";
 import "element-plus/theme-chalk/el-notification.css";
 
