37a91f35907f1faf0ce4bb7752160b845ad2e96f
ZiuChen <457353192@qq.com>
2022-06-27 10:20:18 +0800

    fix: 修正notification无法正确显示的问题

--- src/global/RegisterApp.ts
+++ src/global/RegisterApp.ts
@@ -3,6 +3,7 @@ import App from "../components/App.vue";
 import router from "../route";
 import { ElLoading } from "element-plus/lib/components/loading/index";
 import "element-plus/theme-chalk/el-loading.css";
+import "element-plus/theme-chalk/el-notification.css";
 
 const registerApp = () => {
   const app = createApp(App);
