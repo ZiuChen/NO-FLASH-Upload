26dfbd70a35b765c5c56d3dd1b1617683ac0cfe6
ZiuChen <457353192@qq.com>
2022-11-24 16:42:50 +0800

    feat: 移除外部CDN链接依赖

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import { loadCSS } from "@/utils/LoadResource";
 import isUsingOld from "@/hooks/CheckUsingOld";
 import ConfigOperations from "@/hooks/Config/ConfigOperations";
 import registerApp from "@/global/RegisterApp";
-
-const css = [
-  "https://gcore.jsdelivr.net/npm/@wangeditor/editor@latest/dist/css/style.css",
-];
 
 const atMainPage = window.location.href.indexOf("personal.do") !== -1;
 
@@ -17,8 +12,6 @@ if (!isUsingOld() && atMainPage) {
   });
   // normalize page style
   document.body.style.margin = "0";
-  // insert lib style
-  css.map((style) => loadCSS(style));
   // vue init
   registerApp();
 }
