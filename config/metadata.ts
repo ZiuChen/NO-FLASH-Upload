cfdb981b47b7df46b7a335388f1e85253048e021
ZiuChen <chenziyu03@kuaishou.com>
2023-04-24 14:03:31 +0800

    fix: 替换脚本更新链接

--- config/metadata.ts
+++ config/metadata.ts
@@ -8,9 +8,9 @@ export default {
   "description:en":
     "Beijing Jiaotong University curriculum platform function enhancements, information aggregation, accessories uploading, allowing you to efficient course information.",
   updateURL:
-    "https://service-n4tqsll5-1304937021.sh.apigw.tencentcs.com/release/v1/index.prod.user.js",
+    "https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/publish/index.prod.user.js",
   downloadURL:
-    "https://service-n4tqsll5-1304937021.sh.apigw.tencentcs.com/release/v1/index.prod.user.js",
+    "https://fastly.jsdelivr.net/gh/ZiuChen/NO-FLASH-Upload@master/publish/index.prod.user.js",
   version: version,
   author: author,
   source: repository.url,
